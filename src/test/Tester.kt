5bf34065657ed8091ee3df4094e9be0f2d644d0a
Robin <robin.tordly@nav.no>
2024-10-21 15:10:08 +0200

    refactor color

--- src/test/Tester.kt
+++ src/test/Tester.kt
@@ -17,42 +17,21 @@ fun main(args: Array<String>) {
 annotation class Test
 
 
-// 
-//  
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+//         
 data class TestResult(private val name: String, private val success: Boolean, private val time: Long) {
     private fun status(): String = when (success) {
-        true -> "".green_text() + "PASS".green_bg().gray_text() + "".green_text()
-        false -> "".red_text() + "FAIL".red_bg().gray_text() + "".red_text()
+        true -> "".text(Color.GREEN) + "PASS".bg(Color.GREEN).text(Color.GRAY) + "".text(Color.GREEN)
+        false -> "".text(Color.RED) + "FAIL".bg(Color.RED).text(Color.GRAY) + "".text(Color.RED)
     }
 
-    private fun time(): String = "${time/1_000_000} ms".padEnd(9, ' ').yellow_text()
-
-    private fun name(): String = "$name ".padEnd(40, '˒').white_text()
-
+    private fun time(): String = "${time/1_000_000} ms".padEnd(9, ' ').text(Color.YELLOW)
+    private fun name(): String = "$name ".padEnd(40, '˒').text(Color.WHITE)
 
     override fun toString(): String = " ${time()} ${name()} ${status()}"
 }
 
-
 object ClassLoader {
 
-    // fun runTest(className: String, testName: String): Result<TestResult, Throwable> {
-    //     val testClass = Class.forName(className)
-    //     return testClass.methods
-    //         .filter { it.isTest() }
-    //         .filter { it.name == testName }
-    //         .singleOrNull()?.let { test -> testClass.runTest(test) }
-    //         ?: Result.Err(IllegalArgumentException("Test $testName not found in class $className"))
-    // }
-
-    // fun runTest(clazz: Class<*>, method: Method): Result<TestResult, Throwable> {
     fun runTest(className: String, testName: String): Result<TestResult, Throwable> {
         val testClass = Class.forName(className)
         return testClass.methods
@@ -93,7 +72,7 @@ object ClassLoader {
                 //
                 // val line = "".gray_text() + " $classname ".light_gray_text() + "".gray_text()
                 // println("          ".gray_bg() + line + "          ".gray_bg())
-                println("".gray_text() + classname.gray_bg().light_gray_text() + "".gray_text())
+                println("".text(Color.GRAY) + classname.bg(Color.GRAY).text(Color.LIGHT_GRAY) + "".text(Color.GRAY))
 
                 val c = clazz.getDeclaredConstructor().newInstance()
                 val res = c::class.java.methods
@@ -105,25 +84,23 @@ object ClassLoader {
             }
     }
 }
-// color_blue = '#458588'
-private fun String.blue_text(): String = "${ANSI24bit.fg(69, 133, 136)}$this${ANSI24bit.reset()}"
-private fun String.blue_bg(): String = "${ANSI24bit.bg(69, 133, 136)}$this${ANSI24bit.reset()}"
-// color_green = '#98971a'
-private fun String.yellow_text(): String = "${ANSI24bit.fg(152, 151, 26)}$this${ANSI24bit.reset()}"
-private fun String.yellow_bg(): String = "${ANSI24bit.bg(152, 151, 26)}$this${ANSI24bit.reset()}"
-// color_purple = '#b16286'
-private fun String.purple_text(): String = "${ANSI24bit.fg(177, 98, 134)}$this${ANSI24bit.reset()}"
-private fun String.purple_bg(): String = "${ANSI24bit.bg(177, 98, 134)}$this${ANSI24bit.reset()}"
 
-private fun String.white_text(): String = "${ANSI24bit.fg(222, 222, 222)}$this${ANSI24bit.reset()}"
-private fun String.green_bg(): String = "${ANSI24bit.bg(0, 222, 155)}$this${ANSI24bit.reset()}"
-private fun String.green_text(): String = "${ANSI24bit.fg(0, 222, 155)}$this${ANSI24bit.reset()}"
-private fun String.dark_yellow_text(): String = "${ANSI24bit.fg(100, 100, 0)}$this${ANSI24bit.reset()}"
-private fun String.red_bg(): String = "${ANSI24bit.bg(250, 100, 100)}$this${ANSI24bit.reset()}"
-private fun String.red_text(): String = "${ANSI24bit.fg(250, 100, 100)}$this${ANSI24bit.reset()}"
-private fun String.gray_text(): String = "${ANSI24bit.fg(60, 60, 60)}$this${ANSI24bit.reset()}"
-private fun String.gray_bg(): String = "${ANSI24bit.bg(60, 60, 60)}$this${ANSI24bit.reset()}"
-private fun String.light_gray_text(): String = "${ANSI24bit.fg(135, 125, 125)}$this${ANSI24bit.reset()}"
+data class Color(val r: Int, val b: Int, val g: Int) {
+    companion object {
+        val BLUE = Color(69, 133, 136)
+        val YELLOW = Color(152, 151, 26)
+        val PURPLE = Color(177, 98, 134)
+        val WHITE = Color(222, 222, 222)
+        val GREEN = Color(0, 222, 155)
+        val DARK_YELLOW = Color(100, 100, 0)
+        val RED = Color(250, 100, 100)
+        val GRAY = Color(60, 60, 60)
+        val LIGHT_GRAY = Color(135, 125, 125)
+    }
+}
+
+private fun String.text(color: Color): String = "${ANSI24bit.fg(color.r, color.b, color.g)}$this${ANSI24bit.reset()}"
+private fun String.bg(color: Color): String = "${ANSI24bit.bg(color.r, color.b, color.g)}$this${ANSI24bit.reset()}"
 private fun Long.stop(): Long = (System.nanoTime() - this)
 private fun Method.isTest(): Boolean = isAnnotationPresent(Test::class.java)
 private fun Class<*>.hasTests(): Boolean = methods.any{ it.isTest() }
